faf5d7780a7c5e9c65a16a16984108bd4acdbbe4
dym6295@163.com <dym6295@163.com>
2017-05-08 21:08:33 +0800

    add Member

---
+++ Component/dym.Logic/Platform.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Dapper;
+using dym.Model;
+
+namespace dym.Logic
+{
+    public class Platform : Context.Context
+    {
+        public Platform()
+        {
+        }
+        public List<T> GetList<T>(string where=null)
+        { 
+            return db.Query<T>($"select * from {typeof(T).Name} {where}").AsList<T>();
+        }
+    }
+}
