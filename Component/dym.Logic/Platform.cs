6b42f4256e39cdddd30cc9d81bc0e1bafeaf2c10
dym6295@163.com <dym6295@163.com>
2017-05-09 17:12:12 +0800

    add Delete

--- Component/dym.Logic/Platform.cs
+++ Component/dym.Logic/Platform.cs
@@ -14,5 +14,13 @@ namespace dym.Logic
         { 
             return db.Query<T>($"select * from {typeof(T).Name} {where}").AsList<T>();
         }
+        public Boolean Delete<T>(string Where)
+        {
+            if (string.IsNullOrWhiteSpace(Where))
+            {
+                throw new ArgumentNullException("必须指定 Where 条件！");
+            }
+            return db.Execute($"delete from  {typeof(T).Name} {Where}")>0;
+        }
     }
 }
