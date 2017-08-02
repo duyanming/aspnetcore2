77af3084139d34c1ca47767ffec501e26eb411eb
杜燕明 <dym6295@163.com>
2017-08-02 17:00:35 +0800

    autofac Repository

--- Server/dym.Web/AutofacModule.cs
+++ Server/dym.Web/AutofacModule.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 using Autofac;
 using dym.Repository;
 using dym.IRepository;
+using System.Reflection;
 
 namespace dym.Web
 {
-    public class AutofacModule : Module
+    public class AutofacModule : Autofac.Module
     {
         //注意以下写法
         //builder.RegisterType<GuidTransientAppService>().As<IGuidTransientAppService>();
@@ -23,7 +24,14 @@ namespace dym.Web
             //builder.Register(c => new ValuesService(c.Resolve<ILogger<ValuesService>>()))
             //    .As<IValuesService>()
             //    .InstancePerLifetimeScope();
-            builder.RegisterType<BaseRepository>().As<IBaseRepository>();
+            // builder.RegisterType<BaseRepository>().As<IBaseRepository>();
+
+            AssemblyName assemblyName = new AssemblyName("dym.Repository");
+            Assembly.Load(assemblyName).GetTypes().Where(x => x.GetTypeInfo().IsClass && !x.GetTypeInfo().IsAbstract && !x.GetTypeInfo().IsInterface).ToList().ForEach(
+                _t => {
+                    builder.RegisterType(_t).As(_t.GetInterfaces());
+                }
+            );
         }
     }
 }
