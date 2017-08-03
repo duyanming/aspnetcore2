fefd650cca7ba82535d3484322bf44c6420c9fe9
杜燕明 <dym6295@163.com>
2017-08-03 17:10:51 +0800

    移除logicService

--- Server/dym.Web/Controllers/HomeController.cs
+++ Server/dym.Web/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using dym.LogicService;
 using dym.IRepository;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
