68f642572805bcc6b3b18f84c8c1fdaa1c953b30
杜燕明 <dym6295@163.com>
2017-08-19 19:13:07 +0800

    ViewModel

---
+++ Component/dym.ViewModel/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dym.ViewModel
+{
+    public class IndexViewModel
+    {
+        public string account { get; set; }
+        public string name { get; set; }
+        public string position { get; set; }
+        public DateTime? rdt { get; set; }
+        public long coid { get; set; }
+        public string corpName { get; set; }
+    }
+}
